5f129bcae8ef5374a68cf5ed367375d64e0e95ff
Ljw <132702102+location132@users.noreply.github.com>
2024-05-09 11:55:21 +0900

    fix: 이미지 변경

--- lib/main_detail/main_screen.dart
+++ lib/main_detail/main_screen.dart
@@ -15,24 +15,32 @@ class _MainScreenState extends State<MainScreen> {
     double widthSize = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Padding(
-        padding:
-            EdgeInsets.only(left: widthSize * 0.01, right: widthSize * 0.01),
-        child: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Stack(
-                  children: [
-                    PhoneScreen(),
-                    IdeScreen(),
-                  ],
-                ),
-              ],
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Images/main_bg.png'),
+            fit: BoxFit.cover, // 배경 이미지가 전체 화면을 채우도록 설정
+          ),
+        ),
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: widthSize * 0.01, right: widthSize * 0.01),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    children: [
+                      //PhoneScreen(),
+                      IdeScreen(),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
