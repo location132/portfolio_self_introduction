805797ce4501c7679921e1c3959365339fc0e2bd
Ljw <132702102+location132@users.noreply.github.com>
2025-06-22 23:48:39 +0900

    fix: 모바일 뷰

--- lib/page/tech_blog_page/tech_blog_mobile/widget/mobile_tech_post_title.dart
+++ lib/page/tech_blog_page/tech_blog_mobile/widget/mobile_tech_post_title.dart
@@ -7,6 +7,8 @@ class MobileTechPostTitle extends StatelessWidget {
   final String tags;
   final int number;
   final VoidCallback? onTap;
+  final String? uploadDate;
+  final bool isUpload;
 
   const MobileTechPostTitle({
     super.key,
@@ -14,6 +16,8 @@ class MobileTechPostTitle extends StatelessWidget {
     required this.tags,
     required this.number,
     this.onTap,
+    this.uploadDate,
+    this.isUpload = false,
   });
 
   @override
@@ -40,20 +44,54 @@ class MobileTechPostTitle extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      title,
-                      style: GoogleFonts.montserrat(
-                        color: Colors.white,
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.w600,
-                      ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            title,
+                            style: GoogleFonts.montserrat(
+                              color:
+                                  isUpload
+                                      ? Colors.white
+                                      : Colors.grey.shade600,
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        if (uploadDate != null && !isUpload) ...[
+                          SizedBox(width: 8.w),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 8.w,
+                              vertical: 4.h,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade600,
+                              borderRadius: BorderRadius.circular(4.r),
+                            ),
+                            child: Text(
+                              '$uploadDate 업로드 예정',
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ],
                     ),
                     SizedBox(height: 6.h),
                     Text(
                       tags,
                       style: GoogleFonts.montserrat(
-                        color: Colors.grey.shade400,
-                        fontSize: 12.sp,
+                        color:
+                            isUpload
+                                ? Colors.grey.shade400
+                                : Colors.grey.shade600,
+                        fontSize: 14.sp,
                         fontWeight: FontWeight.w400,
                       ),
                     ),
